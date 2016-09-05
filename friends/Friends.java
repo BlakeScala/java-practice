b8043ef479dcc0ad9eab5f14e6e9ada7cc51aa97
Blake Scala <blakescala@gmail.com>
2016-09-05 16:53:50 -0700

    make an array of friends print out in the console using a for each loop

---
+++ friends/Friends.java
@@ -0,0 +1,9 @@
+public class Friends {
+  public static void main(String[] args) {
+    String[] friends = { "Tristan", "Connor", "Day", "Bob", "Nick", "Christian"};
+
+    for (String friend: friends) {
+      System.out.println(friend + " is my friend.");
+    }
+  }
+}
