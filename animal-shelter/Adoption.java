ed40bc905c08b47210d182c02cd7b52c7895bab2
Blake Scala <blakescala@gmail.com>
2016-09-05 23:26:52 -0700

    ask user the max weight of animals allowed in the house and display animals under that weight

--- animal-shelter/Adoption.java
+++ animal-shelter/Adoption.java
@@ -2,4 +2,9 @@ class Animal {
   public String mName;
   public String mSpecies;
   public int mAge;
+  public int mWeight;
+
+  public boolean insidePet(int maxWeight){
+    return (maxWeight >= mWeight);
+  }
 }
--- animal-shelter/App.java
+++ animal-shelter/App.java
@@ -1,29 +1,42 @@
+import java.io.Console;
+
 public class App {
   public static void main(String[] args) {
-    
+    Console console = System.console();
+
+    System.out.println("What is the max weight of animals allowed in the house?");
+    String stringMaxWeight = console.readLine();
+    Integer maxWeight = Integer.parseInt(stringMaxWeight);
+
     Animal dog = new Animal();
     dog.mName = "Margot";
     dog.mSpecies = "Border Collie";
     dog.mAge = 4;
+    dog.mWeight = 50;
 
     Animal cat = new Animal();
     cat.mName = "Ichi";
     cat.mSpecies = "Cat";
     cat.mAge = 5;
+    cat.mWeight = 28;
 
     Animal pig = new Animal();
     pig.mName = "Appa";
-    pig.mSpecies = "pig";
+    pig.mSpecies = "Pig";
     pig.mAge = 8;
+    pig.mWeight = 180;
 
     Animal[] animals = { dog , cat , pig};
 
-    System.out.println("All animals: ");
+    System.out.println("Here are animals that aren't too heavy: ");
 
     for (Animal eachAnimal : animals) {
-      System.out.println(eachAnimal.mName);
-      System.out.println(eachAnimal.mSpecies);
-      System.out.println(eachAnimal.mAge);
+      if (eachAnimal.insidePet(maxWeight)){
+        System.out.println(eachAnimal.mName);
+        System.out.println(eachAnimal.mSpecies);
+        System.out.println(eachAnimal.mAge);
+        System.out.println(eachAnimal.mWeight);
+      }
     }
   }
 }
